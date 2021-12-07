3eef1888af7311e9069338b1443645b7880e6d00
hyunsu4020 <76948939+hyunsu4020@users.noreply.github.com>
2021-12-07 23:27:09 +0900

    Add files via upload

---
+++ main/6-1.js
@@ -0,0 +1,8 @@
+function over(obj) // function over(obj): 함수 작성입니다. obj는 전달받은 div 태그를 가리킴
+{
+  obj.style.background="yellow"; // div태그 배경 스타일을 노란색으로 합니다.
+}
+function out(obj) // function out(obj): 함수 작성입니다.
+{
+  obj.style.background="white"; // div태그 배경 스타일을 흰색으로 합니다.
+}
